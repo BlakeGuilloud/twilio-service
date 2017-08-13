98b7be738655a7825679baba67a372cafd35c9be
Blake Guilloud <blake.guilloud@gmail.com>
2017-08-13 15:12:45 -0400

    Stripping and shaping the phone numbers.

--- send-message/lib/helpers.js
+++ send-message/lib/helpers.js
@@ -1,3 +1,15 @@
+function stripAndShapePhone(phoneNumber) {
+  let formattedNumber = phoneNumber.replace(/[^0-9]/g, '');
+
+  if (formattedNumber[0] === '1') {
+    formattedNumber = `+${formattedNumber}`;
+  } else {
+    formattedNumber = `+1${formattedNumber}`;
+  }
+
+  return formattedNumber;
+}
+
 function handleSuccess(message) {
   const body = JSON.stringify({
     body: message ? message.body : 'No response body',
@@ -20,9 +32,9 @@ function handleError(err) {
 function fetchPayload({ body, to, from }) {
   return {
     body: body || 'Default body',
-    to: to || '+18438126962',
-    from: from || process.env['FROM_NUMBER']
-  }
+    to: stripAndShapePhone(to) || '+18438126962',
+    from: stripAndShapePhone(from) || process.env['FROM_NUMBER']
+  };
 }
 
 module.exports = {
